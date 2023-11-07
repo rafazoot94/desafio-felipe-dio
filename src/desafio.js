41196f9279c586d9274be452e03289a15fa9fd1c
rafazoot94 <rafaelapsa@outlook.com>
2023-11-07 11:33:52 -0300

    Descrição desafio

---
+++ src/desafio.js
@@ -0,0 +1,23 @@
+
+
+let nomeHeroi = "rafazoot";
+let nivelExperiencia = 550;
+
+if (nivelExperiencia <= 1000) {
+    console.log("O Herói " + nomeHeroi + " está no Nível de: Ferro");
+} else if (nivelExperiencia >= 1001 && nivelExperiencia <= 2000) {
+    console.log("O Herói " + nomeHeroi + " está no Nível de: Bronze");
+} else if (nivelExperiencia >= 2001 && nivelExperiencia <= 5000) {
+    console.log("O Herói " + nomeHeroi + " está no Nível de: Prata");
+} else if (nivelExperiencia >= 5001 && nivelExperiencia <= 6000) {
+    console.log("O Herói " + nomeHeroi + " está no Nível de: Ouro");
+} else if (nivelExperiencia >= 6001 && nivelExperiencia <= 7000) {
+    console.log("O Herói " + nomeHeroi + " está no Nível de: Platina");
+} else if (nivelExperiencia >= 7001 && nivelExperiencia <= 8000) {
+    console.log("O Herói " + nomeHeroi + " está no Nível de: Ascendente");
+} else if(nivelExperiencia >= 8001 && nivelExperiencia <= 9000) {
+    console.log("O Herói " + nomeHeroi + " está no Nível de: Imortal");
+} if (nivelExperiencia > 10000) {
+    console.log("O Herói " + nomeHeroi + " está no Nível de: Radiante");
+}
+
